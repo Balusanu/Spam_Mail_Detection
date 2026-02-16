3367bb2b7a0c5d482f3a923a66b177039ca7ceda
Balasubramanya C K <124988938+Balusanu@users.noreply.github.com>
2026-02-16 13:33:41 +0530

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
 
+# -------- LOAD MODEL --------
+with open("model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI(title="Spam Detection API")
+
+class EmailRequest(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"status": "Spam API running"}
+
+@app.post("/predict")
+def predict(req: EmailRequest):
+    pred = model.predict([req.message])[0]
+    prob = model.predict_proba([req.message])[0]
+
+    return {
+        "prediction": "Ham" if pred == 1 else "Spam",
+        "confidence": float(max(prob))
+    }
